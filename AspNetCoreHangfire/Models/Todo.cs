390666625de802db1dd333e2f6a2bafb3bf68ff6
Vincent Nwonah <vnwonah@outlook.com>
2019-11-04 02:24:15 +0200

    Add project files.

---
+++ AspNetCoreHangfire/Models/Todo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace AspNetCoreHangfire.Models
+{
+    public class Todo
+    {
+        public string Text { get; set; }
+        public DateTime DueDate { get; set; }
+        public bool Completed { get; set; }
+    }
+}
